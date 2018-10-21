2bb2c402652d1cad71927a06067d2dddac808ced
David Yan <daveddd97@hotmail.com>
2018-10-21 20:01:08 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+  $(".page-content-big-text").hide()
+
+  $(".page-content-bg").ready(function() {
+    $(".page-content-big-text").delay(500).fadeIn(2000)
+    $(".small-text").delay(1200).fadeIn(2000)
+});
+ 
+  console.log("dd")
+  
+
+});
+
+function menuButtonClicked() {
+    var header = document.getElementById("header");
+    var cornerIcon = document.getElementById("corner-icon");
+    header.classList.toggle("fullscreen");
+ 
+    if (cornerIcon.classList.contains("fa-bars")) {
+         cornerIcon.classList.replace("fa-bars", "fa-times");
+       }
+       else{
+         cornerIcon.classList.replace("fa-times", "fa-bars");
+       }
+ }
+
+ 
+ function testScroll(){
+  if(window.pageYOffset>150){
+  document.getElementById("scroll").style.opacity = "0"
+  }
+  else {
+    document.getElementById("scroll").style.opacity = "1"
+  }
+
+}
+window.onscroll=testScroll
+
+
+
