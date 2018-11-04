a2a1a21a7710a7c2524249a1c3fc3c2a9289f06c
David Yan <daveddd97@hotmail.com>
2018-11-04 21:20:52 +0100

    more design to index.html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 $( document ).ready(function() {
-  $(".page-content-big-text").hide()
+    $(".page-content-big-text").hide()
+    $("small-text").hide()
 
-  $(".page-content-bg").ready(function() {
-    $(".page-content-big-text").delay(500).fadeIn(2000)
-    $(".small-text").delay(1200).fadeIn(2000)
-});
- 
-  console.log("dd")
-  
-
+    $(".page-content-bg").ready(function() {
+      $(".page-content-big-text").delay(500).fadeIn(2000)
+      $(".small-text").delay(1200).fadeIn(2000)
+    });
 });
 
 function menuButtonClicked() {
@@ -26,13 +23,12 @@ function menuButtonClicked() {
 
  
  function testScroll(){
-  if(window.pageYOffset>150){
-  document.getElementById("scroll").style.opacity = "0"
-  }
-  else {
-    document.getElementById("scroll").style.opacity = "1"
-  }
-
+    if(window.pageYOffset>150){
+    document.getElementById("scroll").style.opacity = "0"
+    }
+    else {
+      document.getElementById("scroll").style.opacity = "1"
+    }
 }
 window.onscroll=testScroll
 
